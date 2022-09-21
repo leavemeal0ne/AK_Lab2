5464111883d5bd5676be72cc10f05da86cadf9e2
leavemeal0ne <vlad.makaruk246@gmail.com>
2022-09-21 13:34:34 +0300

    Divide function

--- calculator.h
+++ calculator.h
@@ -7,6 +7,7 @@ class Calculator
         int Add (double, double);
         int Sub (double, double);
         int Mul (double, double);
+        int Divide(double,double);
 };
 
 #endif//CALCULATOR_H
